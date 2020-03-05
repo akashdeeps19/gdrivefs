cfd540153a6b43f146d3d29640d51d9dede528af
akashdeep-19 <akasdeeps19@gmail.com>
2020-03-05 22:21:03 +0530

    partial mount

--- src/gdfs/drive_facade.py
+++ src/gdfs/drive_facade.py
@@ -5,6 +5,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import io
+import os
 from googleapiclient.http import MediaIoBaseDownload
 
 class driveFacade:
@@ -12,6 +13,32 @@ class driveFacade:
         # If modifying these scopes, delete the file token.pickle.
         self.SCOPES = ['https://www.googleapis.com/auth/drive']
         self.service = None
+        self.extensions = {
+            'application/vnd.ms-excel': 'xls',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx', 
+            'text/xml': 'xml', 
+            'application/vnd.oasis.opendocument.spreadsheet': 'ods', 
+            'text/plain': 'txt', 
+            'application/pdf': 'pdf', 
+            'application/x-httpd-php': 'php', 
+            'image/jpeg': 'jpg', 
+            'image/png': 'png', 
+            'image/gif': 'gif', 
+            'image/bmp': 'bmp', 
+            'application/msword': 'doc', 
+            'text/js': 'js', 
+            'application/x-shockwave-flash': 'swf', 
+            'audio/mpeg': 'mp3', 
+            'application/zip': 'zip', 
+            'application/rar': 'rar', 
+            'application/tar': 'tar', 
+            'application/arj': 'arj', 
+            'application/cab': 'cab', 
+            'text/html': 'htm', 
+            'application/octet-stream': 'default', 
+            'application/vnd.google-apps.document': 'doc',
+            'application/vnd.google-apps.folder': 'folder'
+        }
 
     def authenticate(self):
         creds = None
@@ -42,9 +69,14 @@ class driveFacade:
 
         return items
 
-    def get_file_content(self,file_id,filename = 'filename.zip',verbose = False):
+    def get_file_content(self,file_id = None,item = None,filename = 'filename.zip',verbose = False,path = './'):
+        if(item):
+            file_id = item['id']
+            filename = os.path.join(path,item['name'] + '.' + item['extension'])
         request = self.service.files().get_media(fileId=file_id)
         fh = io.FileIO(filename, mode='w')
+        if item['extension'] == 'doc':
+            return
         downloader = MediaIoBaseDownload(fh, request)
         done = False
         while done is False:
@@ -52,6 +84,7 @@ class driveFacade:
             if verbose:
                 print("Download %d%%." % int(status.progress() * 100))
         return done
+
 
     def get_root_id(self):
         file_metadata = {
@@ -63,23 +96,49 @@ class driveFacade:
         self.service.files().delete(fileId=tempFolderId).execute() # Delete temporary folder
         return myDriveId
 
+    def get_extension(self,mimeType):
+        return self.extensions[mimeType]
+        
 
     def get_all_files(self,parent = 'root'):
         page_token = None
         items = []
         q = f"'{parent}' in parents"
         while True:
+            # try:
             response = self.service.files().list(q=q,
-                                                spaces='drive',
-                                                fields='nextPageToken, files(id, name)',
-                                                pageToken=page_token).execute()
+                                                    spaces='drive',
+                                                    fields='nextPageToken, files(id, name, mimeType)',
+                                                    pageToken=page_token).execute()
+            # except:
+            #     return []
             for file in response.get('files', []):
+                file['extension'] = self.get_extension(file.pop('mimeType'))
                 items.append(file)
             page_token = response.get('nextPageToken', None)
             if page_token is None:
                 break
         return items
 
+    def downloader(self,path,items,verbose = False):
+        for item in items:
+            full_path = os.path.join(path,item['name'])
+            if os.path.lexists(full_path):
+                continue 
+            if item['extension'] == 'folder':
+                    os.mkdir(full_path)
+                    if verbose:
+                        print('success')
+            else:
+                self.get_file_content(item=item,path=path)
+                if verbose:
+                    print('success')
+        
+    def get_item(self,items,name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return False
 
 
 
@@ -98,7 +157,10 @@ def main():
     #         print(u'{0} ({1})'.format(item['name'], item['id']))
 
     # df.get_file_content(items[3]['id'],'img.jpg',True)
-    print(df.get_all_files('1D-a62Ardu8gOnV_88LHtY0tBsC60y1P-'))
+    items = df.get_all_files()
+    print(items)
+    df.downloader('./root',items)
+    
             
 
 if __name__ == '__main__':
